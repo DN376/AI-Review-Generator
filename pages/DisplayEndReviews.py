a735ab17648f571d5a6c0390ed11ffd9f87784db
DN376 <dave030706@gmail.com>
2024-03-22 14:53:06 -0400

    Revamped copy feature

--- pages/DisplayEndReviews.py
+++ pages/DisplayEndReviews.py
@@ -3,6 +3,7 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 from st_copy_to_clipboard import st_copy_to_clipboard
 from streamlit_extras.stateful_button import button
+import textwrap as tw
 
 if 'NUM_REVIEWS' not in st.session_state:
     switch_page("GreetingPage")
@@ -37,19 +38,22 @@ def main():
     """,
         unsafe_allow_html=True,
     )
-    copied = False
     NUM_REVIEWS = st.session_state["NUM_REVIEWS"]
     for i in range(NUM_REVIEWS):
         st.write("Review #" + str(i + 1) + ":")
         st.write("🌟🌟🌟🌟🌟")
         review_text = st.session_state["review#" + str(i + 1)]
-        st.write(review_text)
+        # st.write(review_text)
         copyKey = str(i) + "COPY_BUTTON"
-        st_copy_to_clipboard(review_text)
+        st.code("\n".join(
+            tw.wrap(
+                review_text,
+                width=80,
+            )
+        ), None)
 
-    if copied:
-        st.link_button("Give us a review_text on Google!",
-                       "https://www.google.com/search?client=firefox-b-d&q=gong+cha+davisville#lrd=0x882b3320b8abb05b:0xbff654876b9a8f56,3,,,,")
+    st.link_button("Give us a review on Google!",
+                   "https://www.google.com/search?client=firefox-b-d&q=gong+cha+davisville#lrd=0x882b3320b8abb05b:0xbff654876b9a8f56,3,,,,")
 
 
 main()
